23976170a83bfdcea45e52e04dc454950e063e73
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-12 00:22:24 +0300

    feat: implement login functionality using authentication service, handle ui during api request using a loading spinner, add error card for handle error responses from the api

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import {Component, inject, signal} from '@angular/core';
+import {Router, RouterLink} from '@angular/router';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { LoginUserDTO } from '../../../shared/interfaces/user.interfaces';
+import { HttpErrorResponse } from '@angular/common/http';
+import {AuthService} from '../../../shared/services/auth.service';
+import {LoginUserDTO} from '../../../shared/interfaces/user.interfaces';
+import {ErrorCardComponent} from '../../ui/error-card/error-card.component';
+import {LoadingSpinnerComponent} from '../../ui/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule, ErrorCardComponent, LoadingSpinnerComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   host: {
-    class: 'flex-grow bg-gray-100 dark:bg-gray-900 transition-colors duration-300 p-4'
+    class: 'flex-grow bg-gray-100 dark:bg-gray-900 transition-colors duration-300 p-24'
   }
 })
 export class LoginComponent {
+    private authService = inject(AuthService);
+    private router = inject(Router);
+
+    isLoading = signal<boolean>(false);
+    error = signal<string | null>(null);
     showPassword = false;
 
     togglePasswordVisibility = () => {
@@ -24,11 +33,25 @@ export class LoginComponent {
       password: new FormControl('', Validators.required),
     })
 
-    // TODO: handle submit login
+
     onLogin() {
       if (!this.form.valid) {
         return;
       }
+      this.isLoading.set(true);
+      this.authService.login(this.getLoginCredentials())
+        .subscribe({
+          next: (resp) => {
+            this.isLoading.set(false);
+            this.router.navigate(['../','dashboard']);
+          },
+          error: (err: HttpErrorResponse)  => {
+            this.isLoading.set(false);
+            this.error.set(err.error.message);
+            this.form.reset();
+            console.log(err);
+          }
+        })
     }
 
     private getLoginCredentials(): LoginUserDTO {

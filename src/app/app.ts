a2b28d793e4b0ac7e5d9c5da918d2b14fb3974aa
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-11 13:09:56 +0300

    feat: set up the main layout  - change the components in the main section using angular router  - initial configuration in routes.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './components/layout/header/header.component';
+import {RouterOutlet} from '@angular/router';
+import {FooterComponent} from './components/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  host: {
+    class: 'min-h-screen flex flex-col justify-between bg-gradient-to-r from-gray-100 to-white dark:from-gray-900 dark:to-gray-800 text-gray-900 dark:text-gray-100 transition-colors duration-300'
+  }
 })
 export class App {
-  protected title = 'MyTasksFrontendApp';
 }

f64a32aaf540be23e28449cf5b71c27a4008f552
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-11 15:56:07 +0300

    refactor: simplify tailwind css styling

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +7,7 @@ import {RouterLink} from '@angular/router';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   host: {
-    class: 'flex-grow p-20 bg-gray-100 dark:bg-gray-900 transition-colors duration-300 p-4'
+    class: 'flex-grow bg-gray-100 dark:bg-gray-900 transition-colors duration-300 p-4'
   }
 })
 export class LoginComponent {
--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -9,7 +9,7 @@ import {RouterLink} from '@angular/router';
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
   host: {
-    class: 'flex-grow p-20 bg-gray-100 dark:bg-gray-900 transition-colors duration-300 p-4',
+    class: 'flex-grow bg-gray-100 dark:bg-gray-900 transition-colors duration-300 p-4',
   }
 })
 export class RegisterComponent {

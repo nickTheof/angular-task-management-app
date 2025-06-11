ebf5ffa424a98fb7cf3116a1bbf6daf8a6d4d583
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-11 15:29:00 +0300

    feat: add register component - add mockup - provide functionality in show/hide password/confirm password - add auth route with nested children (login, register)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {LoginComponent} from './components/auth/login/login.component';
+import {RegisterComponent} from './components/auth/register/register.component';
 
 export const routes: Routes = [
   {
@@ -8,8 +9,16 @@ export const routes: Routes = [
     component: HomeComponent,
   },
   {
-    path:'login',
-    component: LoginComponent,
-  }
-
+    path: 'auth',
+    children: [
+      {
+        path:'login',
+        component: LoginComponent,
+      },
+      {
+        path:'register',
+        component: RegisterComponent,
+      }
+    ]
+  },
 ];
---
+++ src/app/components/auth/register/register.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
+  host: {
+    class: 'flex-grow p-20 bg-gray-100 dark:bg-gray-900 transition-colors duration-300 p-4',
+  }
+})
+export class RegisterComponent {
+  showPassword = false;
+  showConfirmPassword = false;
+
+  togglePasswordVisibility = () => {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleConfirmPasswordVisibility = () => {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
+}

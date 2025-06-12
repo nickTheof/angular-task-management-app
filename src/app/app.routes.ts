c51b29873c719e10c247b08bff2a99c60b6f36dc
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-13 00:01:51 +0300

    feat: add routing configuration for routes that requires authentication and authorization - configure canActivate and canActivateChild be using the customed auth guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,12 @@ import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './components/auth/reset-password/reset-password.component';
+import {DashboardComponent} from './components/dashboard/dashboard/dashboard.component';
+import {authGuard, authGuardChild} from './shared/guards/auth.guard';
+import {AdminPanelComponent} from './components/dashboard/admin-panel/admin-panel.component';
+import {adminGuard, adminGuardChild} from './shared/guards/admin.guard';
+import {NewTaskComponent} from './components/tasks/new-task/new-task.component';
+import {TasksComponent} from './components/tasks/tasks/tasks.component';
 
 export const routes: Routes = [
   {
@@ -31,4 +37,27 @@ export const routes: Routes = [
       }
     ]
   },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [authGuard],
+    canActivateChild: [authGuardChild],
+    children: [
+      {
+        path: 'tasks',
+        component: TasksComponent,
+      },
+    ]
+  },
+  {
+    path: 'dashboard/tasks/new-task',
+    component: NewTaskComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'admin-panel',
+    component: AdminPanelComponent,
+    canActivate: [authGuard, adminGuard],
+    canActivateChild: [authGuardChild, adminGuardChild]
+  }
 ];

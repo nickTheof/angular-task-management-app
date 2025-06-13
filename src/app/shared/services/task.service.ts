b02f3b1e74ccbbaab2f4db3979d80aa98b253143
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-13 11:27:14 +0300

    feat: add methods for updating and deleting a current authenticated user's specific task

--- src/app/shared/services/task.service.ts
+++ src/app/shared/services/task.service.ts
@@ -1,7 +1,8 @@
 import {inject, Injectable} from '@angular/core';
 import {environment} from '../../../environments/environment.development';
 import {HttpClient} from '@angular/common/http';
-import {TaskInsertDTO, TaskReadOnlyDTO} from '../interfaces/task.interfaces';
+import {TaskInsertDTO, TaskReadOnlyDTO, TaskUpdateDTO} from '../interfaces/task.interfaces';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,13 @@ export class TaskService {
 
   insertMyTask(dto: TaskInsertDTO) {
     return this.http.post<TaskReadOnlyDTO>(`${environment.BASE_URL}/users/me/tasks`, dto);
+  }
+
+  updateMyTask(taskUuid: string, dto: TaskUpdateDTO): Observable<TaskReadOnlyDTO> {
+    return this.http.patch<TaskReadOnlyDTO>(`${environment.BASE_URL}/users/me/tasks/${taskUuid}`, dto)
+  }
+
+  deleteMyTask(taskUuid: string): Observable<void> {
+    return this.http.delete<void>(`${environment.BASE_URL}/users/me/tasks/${taskUuid}`);
   }
 }

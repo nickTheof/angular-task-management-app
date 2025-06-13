1b2150f528ae19e187d654da026845fed786a7f7
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-06-13 22:08:11 +0300

    feat: add router-outlet in admin panel

--- src/app/components/dashboard/admin-panel/admin-panel.component.ts
+++ src/app/components/dashboard/admin-panel/admin-panel.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {CardResponsesComponent} from '../../ui/card-responses/card-responses.component';
 
 @Component({
   selector: 'app-admin-panel',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterOutlet, CardResponsesComponent],
   templateUrl: './admin-panel.component.html',
   styleUrl: './admin-panel.component.css',
   host: {
